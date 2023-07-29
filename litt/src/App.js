c47a8e5a31cb581083826213d82b88c99a5393b9
Devanshu Kejriwal <devanshukejriwal@Devanshus-MacBook-Air.local>
2023-07-29 15:09:28 +0530

    added my repo

---
+++ litt/src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+
+import Footer from './Footer/Footer'
+import Footer1 from './Footer1'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default App
